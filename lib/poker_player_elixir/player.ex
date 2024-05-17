319e237a7518913050e8818b3ac5007a8af55a4f
Daniel Kozmacs <daniel.kozmacs@digitalnatives.hu>
2024-05-17 10:13:46 +0200

    test

--- lib/poker_player_elixir/player.ex
+++ lib/poker_player_elixir/player.ex
@@ -1,9 +1,12 @@
 defmodule PokerPlayerElixirWeb.Player do
-  @version "0.0.1"
+  @version "0.0.2"
   def version(), do: @version
 
-  def bet_request(_game_state) do
-    0
+  def bet_request(game_state) do
+    IO.inspect(game_state, label: "game_state")
+    IO.inspect(game_state.current_buy_in, label: "current_buy_in")
+
+    game_state.current_buy_in
   end
 
   def showdown(_game_state) do
