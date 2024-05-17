10155f50747913198480e381b44f3eed57284f0c
Daniel Kozmacs <daniel.kozmacs@digitalnatives.hu>
2024-05-17 11:39:15 +0200

    double on pair

--- lib/poker_player_elixir/player.ex
+++ lib/poker_player_elixir/player.ex
@@ -1,12 +1,30 @@
 defmodule PokerPlayerElixirWeb.Player do
-  @version "0.0.2"
+  @version "0.0.3"
   def version(), do: @version
 
   def bet_request(game_state) do
-    IO.inspect(game_state, label: "game_state")
-    IO.inspect(game_state["current_buy_in"], label: "current_buy_in")
+    # high_cards = [
+    #   "10",
+    #   "J",
+    #   "Q",
+    #   "K",
+    #   "A"
+    # ]
 
-    game_state["current_buy_in"]
+    players = game_state["players"]
+    [our_player | _] = players
+    hole_cards = our_player["hole_cards"]
+    ranks = Enum.map(hole_cards, fn card -> card["rank"] end)
+
+    if has_pair(ranks) do
+      game_state["current_buy_in"] * 2
+    else
+      game_state["current_buy_in"]
+    end
+  end
+
+  defp has_pair([card_1, card_2]) do
+    card_1 == card_2
   end
 
   def showdown(_game_state) do
